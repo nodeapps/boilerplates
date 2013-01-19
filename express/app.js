a531fe1cbbaf02655a04d83e1b48dbeb83e291c7
Farrin Reid <blakmatrix@gmail.com>
2013-01-18 16:03:23 -0800

    [dist][express] upgrade w/ link to app url

--- express/app.js
+++ express/app.js
@@ -6,35 +6,41 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , port = 8080
+  , url  = 'http://localhost:' + port + '/';
+/* We can access nodejitsu enviroment variables from process.env */
+/* Note: the SUBDOMAIN variable will always be defined for a nodejitsu app */
+if(process.env.SUBDOMAIN){
+  url = 'http://' + process.env.SUBDOMAIN + '.jit.su/';
+}
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', 8080);
+  app.set('port', process.env.PORT || port);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-
 app.get('/', routes.index);
+app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
+  console.log("Express server listening on port " + app.get('port'));
+  console.log(url);
 });
-
